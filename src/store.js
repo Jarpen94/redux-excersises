a8feed9d3147e88b77f83743ba6e8a96880fbb82
Jarpen94 <kwiatowskijarek@o2.pl>
2018-12-05 11:46:31 +0100

    added reducer

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
-// import reducers here
+import counter from './state/counter'
 
 const reducer = combineReducers({
-  // reducers here
+    counter
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 )
